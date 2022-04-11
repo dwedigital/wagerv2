3abafd0b49fd54719491129a06bbda2b4c0ee71d
dwedigital <dave@dwedigital.com>
2022-04-11 23:09:21 +0100

    added mailgun and updated mail driver

--- app/mailables/NewChallenge.py
+++ app/mailables/NewChallenge.py
@@ -5,7 +5,6 @@ class NewChallenge(Mailable):
     def build(self):
         return (
             self.subject("You've Been Challenged to a Wager")
-            .from_("admin@gmail.com")
             .text("Hello from Masonite!")
             .html("<h1>Hello from Masonite!</h1>")
         )
